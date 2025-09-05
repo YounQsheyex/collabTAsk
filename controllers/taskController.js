a9a12eed6d5b971b2b9e327fcb040336c5340b79
YounQsheyex <younqsheyex@gmail.com>
2025-09-05 08:36:02 -0700

    feat:added create task feat

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,6 +1,20 @@
 const TASK = require("../models/taskmodel");
 
-// create
+// create by seyi
+const createTask = async (req, res) => {
+  try {
+    const newTask = new TASK(req.body);
+    const { title } = newTask;
+    const taskExist = await TASK.findOne({ title });
+    if (taskExist) {
+      res.status(400).json({ message: "Task Already Exist" });
+    }
+    const saveTask = await newTask.save();
+    res.status(201).json({ message: "Task Created Successfully", saveTask });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 // get all task
 
@@ -9,4 +23,4 @@ const TASK = require("../models/taskmodel");
 // edit task
 
 // delete task
-module.exports = {};
+module.exports = { createTask };
